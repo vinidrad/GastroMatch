c0c56e01f59089d1383f56404d43d1b6e946536c
viniciusmenezes-beep <vinicius.menezes@aluno.senai.br>
2026-09-02 09:40:05 -0300

    novo

--- GastroMatch/GastroMatch/obj/Debug/net8.0/GastroMatch.AssemblyInfo.cs
+++ GastroMatch/GastroMatch/obj/Debug/net8.0/GastroMatch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GastroMatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a37bf1f7f497915a5b12697790af0011a120bbfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac7340f8302a00e926391485648ad61491ed58a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GastroMatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GastroMatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
